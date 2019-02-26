fe6cb3f55d407e0db0d08ecbeb13ccdf889c494e
Anton Kechashin <atk@sitecore.net>
2019-02-26 13:44:29 +0200

    Patch for SXA 1.7.1

---
+++ src/Sitecore.Support.313261/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.313261")]
+[assembly: AssemblyProduct("Sitecore.Support.313261")]
+[assembly: ComVisible(false)]
